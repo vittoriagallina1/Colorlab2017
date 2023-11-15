bf714a2b97d716237c760758dbf6c0860282c80d
vittoriagallina1 <vgallina@stu.naperville203.org>
2023-11-15 09:40:14 -0600

    shepherd fairy

--- ColorLabs17/GallinaColorLabs.java
+++ ColorLabs17/GallinaColorLabs.java
@@ -18,10 +18,21 @@ public class GallinaColorLabs
   public static void main(String[] args)
   {
      Picture swan = new Picture("images/swan.jpg");
+     Picture swan2 = new Picture("images/swan.jpg");
+     Picture swan3 = new Picture("images/swan.jpg");
+     Picture swan4 = new Picture("images/swan.jpg");
+     Picture swan5 = new Picture("images/swan.jpg");
+     Picture swan6 = new Picture("images/swan.jpg");
+     Picture swan7 = new Picture("images/swan.jpg");
      swan.explore();
      Pixel[] swanpixels;
      swanpixels = swan.getPixels();
-     
+     Pixel[] swanpixels2 = swan2.getPixels();
+      Pixel[]swanpixels3 = swan3.getPixels();
+      Pixel[] swanpixels4 = swan4.getPixels();
+     Pixel[] swanpixels5 = swan5.getPixels();
+     Pixel[] swanpixels6 = swan6.getPixels();
+     Pixel[] swanpixels7 = swan7.getPixels();
      //doublefactor
      for (Pixel spot1: swanpixels)
      {
@@ -38,14 +49,101 @@ public class GallinaColorLabs
      swan.explore();
      
      //negate
-     for (Pixel spot1: swanpixels)
+     for (Pixel spot2: swanpixels2)
      {
-         int newblue = 255 - spot1.getBlue();
-         int newred = 255 - spot1.getRed();
-         int newgreen = 255 - spot1.getGreen();
+         int newblue = 255 - spot2.getBlue();
+         int newred = 255 - spot2.getRed();
+         int newgreen = 255 - spot2.getGreen();
          Color negatecolor = new Color(newred, newgreen, newblue);
-         spot1.setColor(negatecolor);
+         spot2.setColor(negatecolor);
      }
-     swan.explore();
+     swan2.explore();
+     
+     //greyscale
+     for (Pixel spot3: swanpixels3)
+     {
+         int newblue = spot3.getBlue();
+         int newred = newblue;
+         int newgreen = newblue;
+         Color greyscale = new Color(newred, newgreen, newblue);
+         spot3.setColor(greyscale);
+     }
+     swan3.explore();
+     
+     //lighten
+     for (Pixel spot4: swanpixels4)
+     {
+         int newblue = spot4.getBlue() + 50;
+         int newred = spot4.getRed() + 50;
+         int newgreen = spot4.getGreen() + 50;
+         if (newblue > 255)
+             newblue = 255;
+         if (newred > 255)
+             newred = 255;
+         if (newgreen > 255)
+             newgreen = 255;
+         Color lightencolor = new Color(newred, newgreen, newblue);
+         spot4.setColor(lightencolor);
+     }
+     swan4.explore();
+     
+     //darken
+     for (Pixel spot5: swanpixels5)
+     {
+         int newblue = spot5.getBlue() - 50;
+         int newred = spot5.getRed() - 50;
+         int newgreen = spot5.getGreen() - 50;
+         if (newblue < 0)
+             newblue = 0;
+         if (newred < 0)
+             newred = 0;
+         if (newgreen < 0)
+             newgreen = 0;
+         Color darkencolor = new Color(newred, newgreen, newblue);
+         spot5.setColor(darkencolor);
+     }
+     swan5.explore();
+     
+     //adjustred
+     for (Pixel spot7: swanpixels7)
+     {
+         int newblue = spot7.getBlue();
+         int newred = spot7.getRed() - 50;
+         int newgreen = spot7.getGreen();
+         if (newred < 0)
+             newred = 0;
+         Color adjustred = new Color(newred, newgreen, newblue);
+         spot7.setColor(adjustred);
+     }
+     swan7.explore();
+     
+     /*changecolors
+     for (Pixel spot7: swanpixels)
+     {
+         int newblue = 67;
+         int newred = 23;
+         int newgreen = 56;
+         Color changecolor = new Color(newred, newgreen, newblue);
+         spot7.setColor(changecolor);
+     }
+     swan.explore();*/
+     
+     //blueify
+     for (Pixel spot8: swanpixels6)
+     {
+         int blue = spot8.getBlue();
+         int red = spot8.getRed();
+         int green = spot8.getGreen();
+         if (red > 150)
+         {    
+            int newblue = 255;
+            int newred = 0;
+            int newgreen = 0;
+            Color blueify = new Color(newred, newgreen, newblue);
+            spot8.setColor(blueify);
+        }
+     }
+     swan6.explore();
   }//main
 }//class
+
--- ColorLabs17/SheparFaireyLab.java
+++ ColorLabs17/SheparFaireyLab.java
@@ -25,26 +25,163 @@ public class SheparFaireyLab
          pictObj.explore();
          
          //relative path
-         Picture apic = new Picture("images\\beach.jpg");
+         Picture apic = new Picture("images\\selfie.jpg");
+         apic.explore();
          //change with selfie picture
-         Picture me = new Picture("images/beach.jpg");
-         Picture me1 = new Picture("images/beach.jpg");
-         Picture me2 = new Picture("images/beach.jpg");
+         Picture me = new Picture("images/selfie.jpg");
+         Picture me1 = new Picture("images/selfie.jpg");
+         Picture me2 = new Picture("images/selfie.jpg");
          
-         /**
-          * method 1 change
-          * 
-          */
+         Pixel[] selfie = me.getPixels();
+         Pixel[] selfie2 = me1.getPixels();
+         Pixel[] selfie3 = me2.getPixels();
          
-         /**
-          * method 2 change
-          * 
-          */
+         //method 1 change
+         for (Pixel spot1: selfie)
+         {
+             int blue = spot1.getBlue();
+             int red = spot1.getRed();
+             int green = spot1.getGreen();
+             int greyvalue = (blue + red + green)/3;
+             if (greyvalue >= 0 && greyvalue < 64)
+             {
+                 int newblue = 102;
+                 int newgreen = 0;
+                 int newred = 0;
+                 Color darkblue = new Color(newred, newgreen, newblue);
+                 spot1.setColor(darkblue);
+             }
+             if (greyvalue >= 64 && greyvalue < 128)
+             {
+                 int newblue = 0;
+                 int newgreen = 0;
+                 int newred = 204;
+                 Color redcolor = new Color(newred, newgreen, newblue);
+                 spot1.setColor(redcolor);
+             }
+             if (greyvalue >= 128 && greyvalue < 192)
+             {
+                 int newblue = 255;
+                 int newgreen = 178;
+                 int newred = 102;
+                 Color lightblue = new Color(newred, newgreen, newblue);
+                 spot1.setColor(lightblue);
+             }
+             if (greyvalue >= 192 && greyvalue <= 255)
+             {
+                 int newblue = 240;
+                 int newgreen = 255;
+                 int newred = 255;
+                 Color offwhite = new Color(newred, newgreen, newblue);
+                 spot1.setColor(offwhite);
+             }
+         }
+         me.explore();
+                  
+      
+         //method 2 change
+         int count = 0;
+         int small = 0;
+         int big = 0;
+         for (Pixel spot1: selfie)
+         {
+             int blue = spot1.getBlue();
+             int red = spot1.getRed();
+             int green = spot1.getGreen();
+             int newvalue = (blue + red + green)/3;
+             if (count == 0)
+                 {
+                    small = newvalue;
+                    big = newvalue;
+                 }
+             else if (newvalue > big)
+                 big = newvalue;
+             else if (newvalue < small)
+                 small = newvalue;
+             count++;
+         }
+         for (Pixel spot1: selfie)
+         {
+             int blue = spot1.getBlue();
+             int red = spot1.getRed();
+             int green = spot1.getGreen();
+             int greyvalue = (blue + red + green)/3;
+             if (greyvalue >= small && greyvalue < ((big-small)/4))
+             {
+                 int newblue = 102;
+                 int newgreen = 0;
+                 int newred = 0;
+                 Color darkblue = new Color(newred, newgreen, newblue);
+                 spot1.setColor(darkblue);
+             }
+             if (greyvalue >= ((big-small)/4) && greyvalue < ((big-small)/2))
+             {
+                 int newblue = 0;
+                 int newgreen = 0;
+                 int newred = 204;
+                 Color redcolor = new Color(newred, newgreen, newblue);
+                 spot1.setColor(redcolor);
+             }
+             if (greyvalue >= ((big-small)/2) && greyvalue < (3*(big-small)/4))
+             {
+                 int newblue = 255;
+                 int newgreen = 178;
+                 int newred = 102;
+                 Color lightblue = new Color(newred, newgreen, newblue);
+                 spot1.setColor(lightblue);
+             }
+             if (greyvalue >=  ((big-small)*0.75) && greyvalue <= big)
+             {
+                 int newblue = 240;
+                 int newgreen = 255;
+                 int newred = 255;
+                 Color offwhite = new Color(newred, newgreen, newblue);
+                 spot1.setColor(offwhite);
+             }
+         }
+         me.explore();
          
-         /**
-          * custom color palette
-          */
-
+         //custom color palette
+         for (Pixel spot2: selfie3)
+         {
+             int blue = spot2.getBlue();
+             int red = spot2.getRed();
+             int green = spot2.getGreen();
+             int greyvalue = (blue + red + green)/3;
+             if (greyvalue >= 0 && greyvalue < 64)
+             {
+                 int newblue = 178;
+                 int newgreen = 180;
+                 int newred = 133;
+                 Color bluegreen = new Color(newred, newgreen, newblue);
+                 spot2.setColor(bluegreen);
+             }
+             if (greyvalue >= 64 && greyvalue < 128)
+             {
+                 int newblue = 188;
+                 int newgreen = 218;
+                 int newred = 255;
+                 Color pastelred = new Color(newred, newgreen, newblue);
+                 spot2.setColor(pastelred);
+             }
+             if (greyvalue >= 128 && greyvalue < 192)
+             {
+                 int newblue = 188;
+                 int newgreen = 255;
+                 int newred = 255;
+                 Color yellowgreen = new Color(newred, newgreen, newblue);
+                 spot2.setColor(yellowgreen);
+             }
+             if (greyvalue >= 192 && greyvalue <= 255)
+             {
+                 int newblue = 192;
+                 int newgreen = 144;
+                 int newred = 175;
+                 Color lilac = new Color(newred, newgreen, newblue);
+                 spot2.setColor(lilac);
+             }
+         }
+         me2.explore();
          
     }//main       
 }//class
